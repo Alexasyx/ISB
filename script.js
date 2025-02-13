a4d4e562c210959dfe5a9dc31195b0762349f8a1
Alexasyx <124909887+Alexasyx@users.noreply.github.com>
2025-02-13 15:51:56 +0800

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const heart = document.getElementById('heart');
+const body = document.body;
 
 heart.addEventListener('click', () => {
-    heart.style.animation = 'rotateFast 2s linear forwards';
+    heart.style.animation = 'rotateExpand 4s ease-in-out forwards';
     setTimeout(() => {
+        body.style.backgroundColor = '#ff6699'; // Pink color similar to the heart
         heart.style.display = 'none';
-    }, 2000);
+    }, 4000);
 });
